91e50b03c82295019f90a430d0579b473f6cfa00
nacent_Coder <beth-o@users.noreply.github.com>
2019-01-28 16:33:49 -0700

    testCode

---
+++ main.java
@@ -0,0 +1,33 @@
+package test;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // generate random numbers within 1 to 6
+		
+		int range = 6;
+		int min = 1;
+        for (int i = 0; i < 6; i++) { 
+            int rand = (int)(Math.random() * range) + min; 
+  
+            //Output is different everytime this code is executed 
+            System.out.println(rand); 
+            int randomNu = 8;
+            String color;
+            switch (color) {
+                case 1:  color = "o";
+                         break;
+                case 2:  monthString = "February";
+                         break;
+                case 3:  monthString = "March";
+                         break;
+
+	}
+
+	}
+}
